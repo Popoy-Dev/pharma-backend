2c20543d16caa84a09d578adbca518ce8accf784
Kua <popoykua28@gmail.com>
2023-02-01 14:28:21 +0800

    Session domain

--- config/session.php
+++ config/session.php
@@ -155,7 +155,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', null),
+    'domain' => 'https://phplaravel-932207-3236365.cloudwaysapps.com',
 
     /*
     |--------------------------------------------------------------------------
